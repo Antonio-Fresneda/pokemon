48b8bad3b94825879325047305b031b85e2a54fc
Danielmcabril <127385863+Danielmcabril@users.noreply.github.com>
2023-05-11 22:42:24 +0200

    clase combate

--- src/pokemon/Combate.java
+++ src/pokemon/Combate.java
@@ -1,64 +1,93 @@
 package pokemon;
 
+import java.util.Random;
+ 
+
 public class Combate {
 	
+	private Entrenador ganador;
 	private Entrenador jugador;
 	private Entrenador rival;
-	private Turno turno;
-	private Pokemon pokemonJugadorKO;
-	private Pokemon pokemonRivalKO;
+	private int turno;
+	private int pokemonJugadorKO;
+	private int pokemonRivalKO;
 	
 	public Combate(Entrenador jugador, Entrenador rival, Turno turno, Pokemon pokemonJugadorKO,
 			Pokemon pokemonRivalKO) {
 		super();
 		this.jugador = jugador;
 		this.rival = rival;
-		this.turno = turno;
-		this.pokemonJugadorKO = pokemonJugadorKO;
-		this.pokemonRivalKO = pokemonRivalKO;
+		this.turno = 1;
+		this.pokemonJugadorKO = 0;
+		this.pokemonRivalKO = 0;
 	
 	
 	}
+	
+	
+	public Entrenador getGanador() {
+		return ganador;
+	}
+
+
+
+	public void setGanador(Entrenador ganador) {
+		this.ganador = ganador;
+	}
+
+
 
 	public Entrenador getJugador() {
 		return jugador;
 	}
 
+
+
 	public void setJugador(Entrenador jugador) {
 		this.jugador = jugador;
 	}
+
 
 	public Entrenador getRival() {
 		return rival;
 	}
 
+
+
 	public void setRival(Entrenador rival) {
 		this.rival = rival;
 	}
 
-	public Turno getTurno() {
+
+	public int getTurno() {
 		return turno;
 	}
 
-	public void setTurno(Turno turno) {
+
+	public void setTurno(int turno) {
 		this.turno = turno;
 	}
 
-	public Pokemon getPokemonJugadorKO() {
+
+	public int getPokemonJugadorKO() {
 		return pokemonJugadorKO;
 	}
 
-	public void setPokemonJugadorKO(Pokemon pokemonJugadorKO) {
+
+	public void setPokemonJugadorKO(int pokemonJugadorKO) {
 		this.pokemonJugadorKO = pokemonJugadorKO;
 	}
 
-	public Pokemon getPokemonRivalKO() {
+
+	public int getPokemonRivalKO() {
 		return pokemonRivalKO;
 	}
 
-	public void setPokemonRivalKO(Pokemon pokemonRivalKO) {
+
+	public void setPokemonRivalKO(int pokemonRivalKO) {
 		this.pokemonRivalKO = pokemonRivalKO;
 	}
+
 
 	@Override
 	public String toString() {
@@ -66,6 +95,48 @@ public class Combate {
 				+ pokemonJugadorKO + ", pokemonRivalKO=" + pokemonRivalKO + "]";
 	}
 
-	
-	
+	 public void comenzarCombate() {
+	        Random random = new Random();
+	        int maxNivelRival = rival.obtenerMaxNivelPokemon();
+	        jugador.establecerNivelMaximoEquipo(maxNivelRival);
+
+	        while (pokemonJugadorKO < 6 && pokemonRivalKO < 6) {
+	            System.out.println("Turno " + turno);
+	            System.out.println("Jugador: " + jugador.getNombre());
+	            System.out.println("Rival: " + rival.getNombre());
+
+	            Pokemon pokemonJugador = jugador.elegirPokemon();
+	            Pokemon pokemonRival = rival.elegirPokemon();
+
+	            System.out.println("Jugador envía a " + pokemonJugador.getNombre());
+	            System.out.println("Rival envía a " + pokemonRival.getNombre());
+
+	            // Realizar la batalla entre los Pokémon
+	            realizarBatalla(pokemonJugador, pokemonRival);
+
+	            turno ++;
+	        }
+	 
+	        if (pokemonJugadorKO == 6) {
+	            ganador = rival;
+	            ((Object) jugador).entregarPokédollars(ganador);
+	            jugador.ganarExperiencia(calcularExperiencia());
+	        } else {
+	            ganador = jugador;
+	            ((Object) rival).entregarPokédollars(ganador);
+	            rival.ganarExperiencia(calcularExperiencia());
+	        }
+
+	        System.out.println("El ganador del combate es: " + ganador.getNombre());
+	    }
+	 
+	 
+	 private void realizarBatalla(Pokemon pokemonJugador, Pokemon pokemonRival) {
+	        
+	    }
+	 
+	 private double calcularExperiencia() {
+	        Pokemon pokemonRival = rival.obtenerPokemonActivo();
+	        return (jugador.obtenerNivelPokemonActivo() + pokemonRival.getNivel() * 10) / 4.0;
+	    }
 }
