5d34ab702caa838eb897bd07a5b9a74dec337e74
Antonio-Fresneda <antonio.fresneda.g@gmail.com>
2023-05-16 20:52:08 +0200

    Nuevas Imagenes Pokemon
    -20 nuevas imagenes de pokemon
    -Minimos cambios en interfaz combate
    -Nuevas rutas para las imagenes en el archivo .sql

--- bd/pokemon.sql
+++ bd/pokemon.sql
@@ -149,67 +149,67 @@ INSERT INTO pokemon VALUES (001,002,004,null,'h',5,20,20,20,20,20,20,20,3,1);
 INSERT INTO pokemon VALUES (001,003,007,null,'h',5,20,20,20,20,20,20,20,3,1);
 
 
-INSERT INTO pokedex VALUES (011,"METAPOD","BICHO", null, "https://images.wikidexcdn.net/mwuploads/wikidex/7/7a/latest/20151123222704/Metapod_oro.png",null,10);
+INSERT INTO pokedex VALUES (011,"METAPOD","BICHO", null, "resources/imgPokemon/Metapod.png",null,10);
 
-INSERT INTO pokedex VALUES (012,"BUTTERFREE","BICHO","VOLADOR","https://images.wikidexcdn.net/mwuploads/wikidex/3/3a/latest/20140110162927/Butterfree_V.png",null,null);
+INSERT INTO pokedex VALUES (012,"BUTTERFREE","BICHO","VOLADOR","resources/imgPokemon/Butterfree.png",null,null);
 
-INSERT INTO pokedex VALUES (013,"WEEDLE","BICHO","VENENO","https://images.wikidexcdn.net/mwuploads/wikidex/f/f5/latest/20140111184319/Weedle_V.png",null,7);
+INSERT INTO pokedex VALUES (013,"WEEDLE","BICHO","VENENO","resources/imgPokemon/Weedle.png",null,7);
 
-INSERT INTO pokedex VALUES (014,"KAKUNA","BICHO","VENENO","https://images.wikidexcdn.net/mwuploads/wikidex/c/ca/latest/20140111143932/Kakuna_V.png",null,10);
+INSERT INTO pokedex VALUES (014,"KAKUNA","BICHO","VENENO","resources/imgPokemon/Kakuna.png",null,10);
 
-INSERT INTO pokedex VALUES (015,"BEEDRILL","BICHO","VENENO","https://images.wikidexcdn.net/mwuploads/wikidex/3/3d/latest/20140110162835/Beedrill_V.png",null,null);
+INSERT INTO pokedex VALUES (015,"BEEDRILL","BICHO","VENENO","resources/imgPokemon/Beedrill.png",null,null);
 
-INSERT INTO pokedex VALUES (016,"PIDGEY","NORMAL","VOLADOR","https://images.wikidexcdn.net/mwuploads/wikidex/e/e1/latest/20151123225243/Pidgey_oro.png",null,18);
+INSERT INTO pokedex VALUES (016,"PIDGEY","NORMAL","VOLADOR","resources/imgPokemon/Pidgey.png",null,18);
 
-INSERT INTO pokedex VALUES (017,"PIDGEOTTO","NORMAL","VOLADOR","https://images.wikidexcdn.net/mwuploads/wikidex/6/6b/latest/20151123225227/Pidgeotto_oro.png",null,36);
+INSERT INTO pokedex VALUES (017,"PIDGEOTTO","NORMAL","VOLADOR","resources/imgPokemon/Pidgeotto.png",null,36);
 
-INSERT INTO pokedex VALUES (018,"PIDGEOT","NORMAL","VOLADOR","https://images.wikidexcdn.net/mwuploads/wikidex/9/9f/latest/20151123225204/Pidgeot_oro.png".null,null);
+INSERT INTO pokedex VALUES (018,"PIDGEOT","NORMAL","VOLADOR","resources/imgPokemon/Pidgeot.png".null,null);
 
-INSERT INTO pokedex VALUES (019,"RATTATA","NORMAL",null,"https://images.wikidexcdn.net/mwuploads/wikidex/4/4f/latest/20151123225901/Rattata_oro.png",null,20);
+INSERT INTO pokedex VALUES (019,"RATTATA","NORMAL",null,"resources/imgPokemon/Rattata.png",null,20);
 
-INSERT INTO pokedex VALUES (020,"RATICATE","NORMAL",null,"https://images.wikidexcdn.net/mwuploads/wikidex/a/ad/latest/20151123225848/Raticate_oro.png",null,null);
+INSERT INTO pokedex VALUES (020,"RATICATE","NORMAL",null,"resources/imgPokemon/Raticate.png",null,null);
 
-INSERT INTO pokedex VALUES (021,"SPEAROW","NORMAL","VOLADOR","https://images.wikidexcdn.net/mwuploads/wikidex/d/d9/latest/20151123230729/Spearow_oro.png",null,20);
+INSERT INTO pokedex VALUES (021,"SPEAROW","NORMAL","VOLADOR","resources/imgPokemon/Spearow.png",null,20);
 
-INSERT INTO pokedex VALUES (022,"FEAROW","NORMAL","VOLADOR","https://images.wikidexcdn.net/mwuploads/wikidex/0/0f/latest/20151123191413/Fearow_oro.png",null,null);
+INSERT INTO pokedex VALUES (022,"FEAROW","NORMAL","VOLADOR","resources/imgPokemon/Fearow.png",null,null);
 
-INSERT INTO pokedex VALUES (023,"EKANS","VENENO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/9/9f/latest/20151123191210/Ekans_oro.png",null,22);
+INSERT INTO pokedex VALUES (023,"EKANS","VENENO",null,"resources/imgPokemon/Ekans.png",null,22);
 
-INSERT INTO pokedex VALUES (024,"ARBOK","VENENO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/8/8e/latest/20151123150906/Arbok_oro.png",null,null);
+INSERT INTO pokedex VALUES (024,"ARBOK","VENENO",null,"resources/imgPokemon/Arbok.png",null,null);
 
-INSERT INTO pokedex VALUES (025,"PIKACHU","ELECTRICO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/a/a4/latest/20151123225253/Pikachu_oro.png",null,28);
+INSERT INTO pokedex VALUES (025,"PIKACHU","ELECTRICO",null,"resources/imgPokemon/Pikachu.png",null,28);
 
-INSERT INTO pokedex VALUES (026,"RAICHU","ELECTRICO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/5/58/latest/20151123225813/Raichu_oro.png",null,null);
+INSERT INTO pokedex VALUES (026,"RAICHU","ELECTRICO",null,"resources/imgPokemon/Raichu.png",null,null);
 
-INSERT INTO pokedex VALUES (027,"SANDSHREW","TIERRA",null,"https://images.wikidexcdn.net/mwuploads/wikidex/c/c4/latest/20151123230055/Sandshrew_oro.png",null,22);
+INSERT INTO pokedex VALUES (027,"SANDSHREW","TIERRA",null,"resources/imgPokemon/Sandshrew.png",null,22);
 
-INSERT INTO pokedex VALUES (028,"SANDSLASH","tierra",null,"https://images.wikidexcdn.net/mwuploads/wikidex/7/7d/latest/20151123230045/Sandslash_oro.png"null,null);
+INSERT INTO pokedex VALUES (028,"SANDSLASH","TIERRA",null,"resources/imgPokemon/Sandslash.png"null,null);
 
-INSERT INTO pokedex VALUES (029,"NIDORAN ♀","VENENO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/1/12/latest/20151123222926/Nidoran_hembra_oro.png",null,16);
+INSERT INTO pokedex VALUES (029,"NIDORAN ♀","VENENO",null,"resources/imgPokemon/Nidoran_hembra.png",null,16);
 
-INSERT INTO pokedex VALUES (030,"NIDORINA","VENENO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/9/91/latest/20151123222944/Nidorina_oro.png",null,32);
+INSERT INTO pokedex VALUES (030,"NIDORINA","VENENO",null,"resources/imgPokemon/Nidorina.png",null,32);
 
-INSERT INTO pokedex VALUES (031,"NIDOQUEEN","VENENO","TIERRA","https://images.wikidexcdn.net/mwuploads/wikidex/6/65/latest/20151123222914/Nidoqueen_oro.png",null,null);
+INSERT INTO pokedex VALUES (031,"NIDOQUEEN","VENENO","TIERRA","resources/imgPokemon/Nidoqueen.png",null,null);
 
-INSERT INTO pokedex VALUES (032,"NIDORAN ♂","VENENO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/2/24/latest/20151123222936/Nidoran_macho_oro.png",null,16);
+INSERT INTO pokedex VALUES (032,"NIDORAN ♂","VENENO",null,"resources/imgPokemon/Nidoran_macho.png",null,16);
 
-INSERT INTO pokedex VALUES (033,"NIDORINO","VENENO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/7/75/latest/20151123222953/Nidorino_oro.png",null,32);
+INSERT INTO pokedex VALUES (033,"NIDORINO","VENENO",null,"resources/imgPokemon/Nidorino.png",null,32);
 
-INSERT INTO pokedex VALUES (034,"NIDOKING","VENENO","TIERRA","https://images.wikidexcdn.net/mwuploads/wikidex/a/a5/latest/20151123222906/Nidoking_oro.png",null,null);
+INSERT INTO pokedex VALUES (034,"NIDOKING","VENENO","TIERRA","resources/imgPokemon/Nidoking.png",null,null);
 
-INSERT INTO pokedex VALUES (035,"CLEFAIRY","HADA",null,"https://images.wikidexcdn.net/mwuploads/wikidex/b/b3/latest/20151123190116/Clefairy_oro.png",null,4);
+INSERT INTO pokedex VALUES (035,"CLEFAIRY","HADA",null,"resources/imgPokemon/Clefairy.png",null,4);
 
-INSERT INTO pokedex VALUES (036,"CLEFABLE","HADA",null,"https://images.wikidexcdn.net/mwuploads/wikidex/a/ab/latest/20151123190106/Clefable_oro.png",null,null);
+INSERT INTO pokedex VALUES (036,"CLEFABLE","HADA",null,"resources/imgPokemon/Clefable.png",null,null);
 
-INSERT INTO pokedex VALUES (037,"VULPIX","FUEGO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/2/23/latest/20151123231509/Vulpix_oro.png",null,4);
+INSERT INTO pokedex VALUES (037,"VULPIX","FUEGO",null,"resources/imgPokemon/Vulpix.g",null,4);
 
-INSERT INTO pokedex VALUES (038,"NINETALES","FUEGO",null,"https://images.wikidexcdn.net/mwuploads/wikidex/7/71/latest/20151123223013/Ninetales_oro.png",null,null);
+INSERT INTO pokedex VALUES (038,"NINETALES","FUEGO",null,"resources/imgPokemon/Ninetales.png",null,null);
 
-INSERT INTO pokedex VALUES (039,"JIGGLYPUFF","NORMAL","HADA","https://images.wikidexcdn.net/mwuploads/wikidex/2/26/latest/20151123193510/Jigglypuff_oro.png",null,null);
+INSERT INTO pokedex VALUES (039,"JIGGLYPUFF","NORMAL","HADA","resources/imgPokemon/Jigglypuffg.png",null,null);
 
-INSERT INTO pokedex VALUES (040,"WIGGLITUFF","NORMAL","HADA","https://images.wikidexcdn.net/mwuploads/wikidex/4/4e/latest/20151123231533/Wigglytuff_oro.png",null,null);
+INSERT INTO pokedex VALUES (040,"WIGGLITUFF","NORMAL","HADA","resources/imgPokemon/Wigglituff.png",null,null);
 
-INSERT INTO pokedex VALUES (041,"ZUBAT","VENENO","VOLADOR","https://images.wikidexcdn.net/mwuploads/wikidex/a/a5/latest/20151123231631/Zubat_oro.png");
+INSERT INTO pokedex VALUES (041,"ZUBAT","VENENO","VOLADOR","resources/imgPokemon/Zubat.png",null,22);
 
 INSERT INTO pokedex VALUES (042,"");
 
